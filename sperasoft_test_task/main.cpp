d1d19f5144f960bcdd33164c818eea0c1d73b90a
Evgeny Kuryatov <evgeny.kuryatov@netcracker.com>
2021-10-14 14:33:41 +0300

    Initial Commit

---
+++ sperasoft_test_task/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sperasoft_test_task
+//
+//  Created by Евгений Курятов on 14.10.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
